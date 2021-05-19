c00ee21559df017fa4fd4a71d57cdc635cd3980b
DMC2712 <chavdadhruv0505@gmail.com>
2021-05-19 14:28:10 +0530

    Added navigations to menu-items using withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { Route, Switch } from "react-router";
 import "./App.css";
 import Homepage from "./pages/homepage/homepage";
+
+const Error404 = (props) => {
+    console.log(props);
+    return (
+        <div>
+            <h1>404 Not found</h1>
+        </div>
+    );
+};
 
 function App() {
     return (
         <div>
-            <Homepage />
+            <Switch>
+                <Route exact path='/' component={Homepage} />
+                <Route exact component={Error404} />
+            </Switch>
         </div>
     );
 }
