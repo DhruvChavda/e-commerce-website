48b9c6917bc68f18ed80eb7bae4d4da94394782a
DMC2712 <chavdadhruv0505@gmail.com>
2021-05-23 15:50:10 +0530

    SignUp component added Custom email can now be stored in the database

--- src/App.js
+++ src/App.js
@@ -28,10 +28,23 @@ export default class App extends Component {
     unsubscribeFromAuth = null;
 
     componentDidMount() {
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(async (user) => {
-            // this.setState({ currentUser: user });
-            createUserProfileDocument(user);
-            // console.log(user);
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+            if (userAuth) {
+                const userRef = await createUserProfileDocument(userAuth);
+
+                userRef.onSnapshot((snapShot) => {
+                    this.setState(
+                        {
+                            currentUser: { id: snapShot.id, ...snapShot.data() },
+                        },
+                        () => console.log("if-block", this.state) //remove this log later
+                    );
+                });
+            } else {
+                this.setState({ currentUser: userAuth }, () =>
+                    console.log("else-block", this.state)
+                ); //remove this log later
+            }
         });
     }
 
--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -28,8 +28,8 @@ export default class SignIn extends Component {
     render() {
         return (
             <div className='sign-in'>
-                <h2>I Already Have an Account</h2>
-                <span>SignIn with your email and password</span>
+                <h2 className='title'>I Already Have an Account</h2>
+                <span>SignIn with your Email and Password</span>
                 <form onSubmit={this.handleSubmit}>
                     <FormInput
                         name='email'
@@ -48,8 +48,8 @@ export default class SignIn extends Component {
                         required
                     />
                     <div className='buttons'>
-                        <CustomButton type='submit'> Sign in </CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                        <CustomButton type='submit'> Sign In </CustomButton>
+                        <CustomButton type='button' onClick={signInWithGoogle} isGoogleSignIn>
                             Sign in with Google
                         </CustomButton>
                     </div>
