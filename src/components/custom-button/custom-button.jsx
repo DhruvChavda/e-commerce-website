6cfa938de32232bbac22237cfee39ee996edc210
DMC2712 <chavdadhruv0505@gmail.com>
2021-06-01 00:05:19 +0530

    Added Add to cart Nutton

--- src/components/custom-button/custom-button.jsx
+++ src/components/custom-button/custom-button.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./custom-button.scss";
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherButtonProps }) => {
+const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherButtonProps }) => {
     return (
         <button
-            className={`${isGoogleSignIn ? "google-sign-in" : ""} custom-button`}
+            className={`${inverted ? "inverted" : ""} ${
+                isGoogleSignIn ? "google-sign-in" : ""
+            } custom-button`}
             {...otherButtonProps}>
             {children}
         </button>
