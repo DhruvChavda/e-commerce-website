fb10a02532f43bf7af15f35ee2aafabaa6d29464
DMC2712 <chavdadhruv0505@gmail.com>
2021-06-01 01:11:15 +0530

    Added cart-utils to write a function that adds quantity to our cart items

--- src/redux/cart/cart-actions.js
+++ src/redux/cart/cart-actions.js
@@ -1,8 +1,8 @@
 import cartActionTypes from "./cart-types";
 
-const toggleCartHidden = () => ({
+export const toggleCartHidden = () => ({
     type: cartActionTypes.TOGGLE_CART_HIDDEN,
     // There is NO need for payload here as we are not using payload in reducer to change value
 });
 
-export default toggleCartHidden;
+export const addItem = (item) => ({ type: cartActionTypes.ADD_ITEM, payload: item });
