d9ec2d16888de8a091afef499ccbdfd39abde33e
DMC2712 <chavdadhruv0505@gmail.com>
2021-06-01 16:22:27 +0530

    Added cart-item component dropdown display done minor error when added multiple item in cart : RESOLVED

--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -1,14 +1,22 @@
 import React from "react";
+import { connect } from "react-redux";
+import CartItem from "../cart-item/cart-item";
 import CustomButton from "../custom-button/custom-button";
 import "./cart-dropdown.scss";
 
-const CartDD = () => {
-    return (
-        <div className='cart-dropdown'>
-            <div className='cart-items'></div>
-            <CustomButton>Go To CheckOut</CustomButton>
+const CartDD = ({ cartItems }) => (
+    <div className='cart-dropdown'>
+        <div className='cart-items'>
+            {cartItems.map((cartItem) => (
+                <CartItem key={cartItem.id} item={cartItem} />
+            ))}
         </div>
-    );
-};
+        <CustomButton>GO TO CHECKOUT</CustomButton>
+    </div>
+);
 
-export default CartDD;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    cartItems,
+});
+
+export default connect(mapStateToProps)(CartDD);
