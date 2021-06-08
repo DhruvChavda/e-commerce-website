3e0a9ebd6f996b3adfe955dcb20dde91a2daf8b0
DMC2712 <chavdadhruv0505@gmail.com>
2021-06-08 16:37:05 +0530

    params mapped

--- src/pages/collection/collection.jsx
+++ src/pages/collection/collection.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { connect } from "react-redux";
+import { selectCollection } from "../../redux/shop/shop-selectors";
 
 import "./collection.scss";
 
-const Collection = ({ match }) => {
-    console.log("**", match);
+const Collection = ({ collection }) => {
+    console.log("collection", collection);
     return (
         <div className='collection-page'>
             <h2>CATEGORY PAGE</h2>
@@ -11,4 +13,9 @@ const Collection = ({ match }) => {
     );
 };
 
-export default Collection;
+const mapStateToProps = (state, ownProps) => {
+    console.log(ownProps.match.params.collectionId);
+    return { collection: selectCollection(ownProps.match.params.collectionId)(state) };
+};
+
+export default connect(mapStateToProps)(Collection);
