d61c2fa2d61f639f3b7eb6c342fcdb561cb788d7
DMC2712 <chavdadhruv0505@gmail.com>
2021-06-09 00:58:59 +0530

    created selectors directory selectors added shop data converted to array

--- src/components/collections-overview/collections-overview.jsx
+++ src/components/collections-overview/collections-overview.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
-import { selectCollections } from "../../redux/shop/shop-selectors";
+import { selectCollectionsForPreview } from "../../redux/shop/shop-selectors";
 
 import CollectionPreview from "../collection-preview/collection-preview";
 
@@ -20,7 +20,7 @@ const CollectionsOverview = ({ collections, match }) => {
 
 // const mapStateToProps = (state) => state.shop;
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollections,
+    collections: selectCollectionsForPreview,
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
--- src/pages/collection/collection.jsx
+++ src/pages/collection/collection.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import { connect } from "react-redux";
 import { selectCollection } from "../../redux/shop/shop-selectors";
 
+import CollectionItem from "../../components/collection-item/collection-item";
+
 import "./collection.scss";
 
 const Collection = ({ collection }) => {
     console.log("collection", collection);
+    const { title, items } = collection;
     return (
         <div className='collection-page'>
-            <h2>CATEGORY PAGE</h2>
+            <h2 className='title'>{title}</h2>
+            <div className='items'>
+                {items.map((item) => (
+                    <CollectionItem key={item.id} item={item} />
+                ))}
+            </div>
         </div>
     );
 };
